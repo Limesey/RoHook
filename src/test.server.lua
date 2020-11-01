d54bd844522878d8803dbf362a18a8221c89265e
Norberto A <norbertofalves@gmail.com>
2020-11-01 16:55:12 +0000

    Removed test file

--- src/test.server.lua
+++
@@ -1,5 +0,0 @@
-local classes = script.Parent.Classes
-local RichEmbedClass = require(classes.RichEmbed)
-
-local embed = RichEmbedClass.new("Hi", "Bunny")
-local field = embed:addField("Test field", "its value")
